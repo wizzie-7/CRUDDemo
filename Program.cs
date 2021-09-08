138c8705ed11e955ab43e83e7733c7a57ffd6134
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-08 12:01:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DatabaseDemo;
+
+namespace CRUDDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 C = new Class1();
+            C.DisplayEmployees();
+            
+            C.AddEmpliyee();
+            C.DisplayEmployees();
+            
+            C.EditEmployee();
+            C.DisplayEmployees();
+            
+            C.DeleteEmployee();
+            C.DisplayEmployees();
+        }
+    }
+}
